8fa1fb60f7fc8204a5dface7be1b73cb5b72f5a9
Mlokiko <mlokos225@gmail.com>
2024-06-17 22:45:37 +0200

    Zmieniono interfejs odczytywania wiadomości

--- ReadMessagesForm.Designer.cs
+++ ReadMessagesForm.Designer.cs
@@ -29,14 +29,18 @@
         private void InitializeComponent()
         {
             splitContainer1 = new SplitContainer();
-            usersDataGridView = new DataGridView();
-            messagesdDataGridView = new DataGridView();
             label1 = new Label();
+            labelFriends = new Label();
+            findingFriendsButton = new Button();
+            FriendsBox = new ListBox();
+            friendNameTextBox = new TextBox();
+            label2 = new Label();
+            messagesdDataGridView = new DataGridView();
+            listboxfriends = new ListBox();
             ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
             splitContainer1.Panel1.SuspendLayout();
             splitContainer1.Panel2.SuspendLayout();
             splitContainer1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)usersDataGridView).BeginInit();
             ((System.ComponentModel.ISupportInitialize)messagesdDataGridView).BeginInit();
             SuspendLayout();
             // 
@@ -48,40 +52,106 @@
             // 
             // splitContainer1.Panel1
             // 
-            splitContainer1.Panel1.Controls.Add(usersDataGridView);
+            splitContainer1.Panel1.Controls.Add(label1);
+            splitContainer1.Panel1.Controls.Add(labelFriends);
+            splitContainer1.Panel1.Controls.Add(findingFriendsButton);
+            splitContainer1.Panel1.Controls.Add(FriendsBox);
+            splitContainer1.Panel1.Controls.Add(friendNameTextBox);
             // 
             // splitContainer1.Panel2
             // 
+            splitContainer1.Panel2.Controls.Add(label2);
             splitContainer1.Panel2.Controls.Add(messagesdDataGridView);
-            splitContainer1.Panel2.Controls.Add(label1);
             splitContainer1.Size = new Size(982, 541);
             splitContainer1.SplitterDistance = 327;
             splitContainer1.TabIndex = 0;
             // 
-            // usersDataGridView
+            // label1
             // 
-            usersDataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            usersDataGridView.Location = new Point(0, 0);
-            usersDataGridView.Name = "usersDataGridView";
-            usersDataGridView.Size = new Size(333, 541);
-            usersDataGridView.TabIndex = 0;
+            label1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label1.AutoSize = true;
+            label1.Location = new Point(32, 441);
+            label1.Name = "label1";
+            label1.Size = new Size(261, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Wybierz użytkownika, aby odczytać konwersacje";
+            // 
+            // labelFriends
+            // 
+            labelFriends.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            labelFriends.AutoSize = true;
+            labelFriends.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            labelFriends.Location = new Point(29, 25);
+            labelFriends.Name = "labelFriends";
+            labelFriends.Size = new Size(108, 21);
+            labelFriends.TabIndex = 4;
+            labelFriends.Text = "Twoi znajomi:";
+            // 
+            // findingFriendsButton
+            // 
+            findingFriendsButton.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            findingFriendsButton.Location = new Point(29, 488);
+            findingFriendsButton.Name = "findingFriendsButton";
+            findingFriendsButton.Size = new Size(272, 23);
+            findingFriendsButton.TabIndex = 3;
+            findingFriendsButton.Text = "Znajdź wiadomości";
+            findingFriendsButton.UseVisualStyleBackColor = true;
+            findingFriendsButton.Click += button1_Click;
+            // 
+            // FriendsBox
+            // 
+            FriendsBox.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            FriendsBox.FormattingEnabled = true;
+            FriendsBox.ItemHeight = 15;
+            FriendsBox.Location = new Point(32, 59);
+            FriendsBox.Name = "FriendsBox";
+            FriendsBox.Size = new Size(272, 304);
+            FriendsBox.TabIndex = 2;
+            // 
+            // friendNameTextBox
+            // 
+            friendNameTextBox.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            friendNameTextBox.Location = new Point(29, 459);
+            friendNameTextBox.Name = "friendNameTextBox";
+            friendNameTextBox.PlaceholderText = "Wpisz imię znajomego";
+            friendNameTextBox.Size = new Size(272, 23);
+            friendNameTextBox.TabIndex = 1;
+            friendNameTextBox.TextAlign = HorizontalAlignment.Center;
+            friendNameTextBox.TextChanged += textBox1_TextChanged;
+            friendNameTextBox.Enter += textBox1_TextChanged;
+            friendNameTextBox.KeyPress += textBox1_KeyPress;
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label2.AutoSize = true;
+            label2.ForeColor = SystemColors.ControlDarkDark;
+            label2.Location = new Point(15, 517);
+            label2.Name = "label2";
+            label2.Size = new Size(633, 15);
+            label2.TabIndex = 5;
+            label2.Text = "(Gdy wybierzesz swoją nazwę użytkownika zostaną wyświetlone wszystkie wiadomości wysłane do Ciebie i przez Ciebie)";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // messagesdDataGridView
             // 
+            messagesdDataGridView.AllowUserToAddRows = false;
+            messagesdDataGridView.AllowUserToDeleteRows = false;
+            messagesdDataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            messagesdDataGridView.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
             messagesdDataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            messagesdDataGridView.Location = new Point(8, -3);
+            messagesdDataGridView.Location = new Point(26, 25);
             messagesdDataGridView.Name = "messagesdDataGridView";
-            messagesdDataGridView.Size = new Size(648, 541);
-            messagesdDataGridView.TabIndex = 1;
+            messagesdDataGridView.ReadOnly = true;
+            messagesdDataGridView.Size = new Size(597, 457);
+            messagesdDataGridView.TabIndex = 3;
             // 
-            // label1
+            // listboxfriends
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(200, 265);
-            label1.Name = "label1";
-            label1.Size = new Size(290, 15);
-            label1.TabIndex = 0;
-            label1.Text = "Wybierz użytkownika aby odczytać konwersacje z nim";
+            listboxfriends.Location = new Point(0, 0);
+            listboxfriends.Name = "listboxfriends";
+            listboxfriends.Size = new Size(120, 96);
+            listboxfriends.TabIndex = 0;
             // 
             // ReadMessagesForm
             // 
@@ -92,20 +162,25 @@
             Name = "ReadMessagesForm";
             Text = "Read Messages";
             splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel1.PerformLayout();
             splitContainer1.Panel2.ResumeLayout(false);
             splitContainer1.Panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
             splitContainer1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)usersDataGridView).EndInit();
             ((System.ComponentModel.ISupportInitialize)messagesdDataGridView).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
+        private ListBox listboxfriends;
         private SplitContainer splitContainer1;
         private Label label1;
-        private DataGridView usersDataGridView;
+        private TextBox friendNameTextBox;
+        private Button findingFriendsButton;
+        private ListBox FriendsBox;
+        private Label labelFriends;
         private DataGridView messagesdDataGridView;
+        private Label label2;
     }
 }
--- ReadMessagesForm.cs
+++ ReadMessagesForm.cs
@@ -9,6 +9,9 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.ComponentModel.Design.ObjectSelectorEditor;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace WinFormsTest3
 {
@@ -17,8 +20,63 @@ namespace WinFormsTest3
         public ReadMessagesForm()
         {
             InitializeComponent();
-            usersDataGridView.DataSource = DBconnection.GetData($"SELECT username from View_{DBconnection.user_name}_read_users");
-            messagesdDataGridView.DataSource = DBconnection.GetData($"SELECT message_text_content, message_date, sender_id, receiver_id FROM View_{DBconnection.user_name}_list_messages");
+
+            var con = new NpgsqlConnection($"Server={DBconnection.server};Port={DBconnection.port};Database={DBconnection.database};Username={DBconnection.user_name_lower};Password={DBconnection.user_password}");
+            var cmd = new NpgsqlCommand($"SELECT DISTINCT username FROM View_{DBconnection.user_name}_read_users NATURAL JOIN View_{DBconnection.user_name}_read_friends where status = 'accepted';", con);
+            con.Open();
+            NpgsqlDataReader reader = cmd.ExecuteReader();
+            var lista = new List<string>();
+            //int i = 0;
+            while (reader.Read())
+            {
+                lista.Add(reader.GetString(0));
+            }
+            FriendsBox.DataSource = lista;
+            con.Close();
+
+            //usersDataGridView.DataSource = DBconnection.GetData($"SELECT DISTINCT username FROM View_{DBconnection.user_name}_read_users NATURAL JOIN View_{DBconnection.user_name}_read_friends where status='accepted';");
+            //FriendsBox.DataSource = DBconnection.GetData($"SELECT DISTINCT username FROM View_{DBconnection.user_name}_read_users NATURAL JOIN View_{DBconnection.user_name}_read_friends where status='accepted';");
+        }
+
+        private void usersDataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void messagesdDataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //usersDataGridView.DataSource = DBconnection.GetData($"SELECT username FROM View_{DBconnection.user_name}_read_users NATURAL JOIN View_{DBconnection.user_name}_read_friends where status='accepted';");
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //messagesdDataGridView.DataSource = DBconnection.GetData($"SELECT message_text_content, message_date, sender_id, receiver_id FROM View_{DBconnection.user_name}_list_messages");
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string friendName = friendNameTextBox.Text;
+            var friendId = DBconnection.NameToId(friendName);
+
+            if (friendName.Length == 0)
+                MessageBox.Show("Proszę wprowadzić nazwę znajomego");
+            else if (friendId == 0)
+                MessageBox.Show("Taki użytkownik nie istnieje lub nie masz takiego znajomego");
+            else
+                messagesdDataGridView.DataSource = DBconnection.GetData($"SELECT message_date AS \"Data wysłania\", message_text_content AS \"Wiadomość\" FROM View_{DBconnection.user_name}_list_messages vlm JOIN View_{DBconnection.user_name}_read_users vu ON (vlm.sender_id=vu.user_id OR vlm.receiver_id=vu.user_id) WHERE vu.username='{friendName}' ORDER BY message_date ASC;");
+
+            //messagesdDataGridView.DataSource = DBconnection.GetData($"SELECT message_text_content, message_date, sender_id, receiver_id FROM View_{DBconnection.user_name_lower}_list_messages WHERE sender_id=(SELECT user_id FROM View_{DBconnection.user_name_lower}_read_users WHERE username={friendName}) OR receiver_id=(SELECT user_id FROM View_{DBconnection.user_name_lower}_read_users WHERE username={friendName})");
         }
     }
 }
